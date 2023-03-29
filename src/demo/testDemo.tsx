40143122d24d534bc8c3c3604f198287b57e9724
zhechen12 <zhechen12@iflytek.com>
2023-03-29 15:57:16 +0800

    fix:测试

--- src/demo/testDemo.tsx
+++ src/demo/testDemo.tsx
@@ -4,7 +4,7 @@ import {bindHook, utils} from 'log';
 const {log, COLOR: {SCHEDULE_COLOR, RENDER_COLOR, COMMIT_COLOR}} = utils;
 
 
-// 用于调试 Reconciler 工作流程的Demo
+// 用于调试 Reconciler 工作流程的Demo 
 export default function App() {
   const Child = () => <div>I am child.</div>;
 
